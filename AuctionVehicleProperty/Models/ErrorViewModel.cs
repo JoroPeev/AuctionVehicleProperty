a9624fd402687bd83d6e3515d69b858f64035bf6
JoroPeev <107910666+JoroP24@users.noreply.github.com>
2024-02-26 21:56:25 +0200

    Add project files.

---
+++ AuctionVehicleProperty/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AuctionVehicleProperty.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
