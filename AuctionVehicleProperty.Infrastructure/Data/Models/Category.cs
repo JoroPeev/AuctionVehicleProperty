4189ddabbed22d25c3d922d735967b1399ced7c7
JoroPeev <107910666+JoroP24@users.noreply.github.com>
2024-03-04 15:12:03 +0200

    Adding class for Make and Category

---
+++ AuctionVehicleProperty.Infrastructure/Data/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace AuctionVehicleProperty.Infrastructure.Data.Models
+{
+    public class Category
+    {
+        [Key]
+        [Comment("VehicleType Identifier")]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Comment("VehicleType Name")]
+        public string Name { get; set; } = string.Empty;
+
+        public IEnumerable<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
+    }
+}
--- AuctionVehicleProperty.Infrastructure/Data/Models/Vehicle.cs
+++ AuctionVehicleProperty.Infrastructure/Data/Models/Vehicle.cs
@@ -12,27 +12,28 @@ namespace AuctionVehicleProperty.Infrastructure.Data.Models
         [Comment("Vehicle Identifier")]
         public int Id { get; set; }
 
+
         [Required]
         [StringLength(100)]
         [Comment("Vehicle Title")]
         public string Title { get; set; } = string.Empty;
 
-        [Required]
-        [Comment("Used or brand new")]
-        public Condition Condition { get; set; }
 
         [Required]
         [Comment("VehicleType Identyfier")]
         public int VehicleTypeId { get; set; }
 
+
         [Required]
         [Comment("Vehicle Type")]
         [ForeignKey(nameof(VehicleTypeId))]
-        public VehicleType VehicleType { get; set; } = null!;
+        public Category VehicleType { get; set; } = null!;
+
 
         [Required]
         [Comment("FuelType Identifier")]
         public int FuelTypeId { get; set; }
+
 
         [Required]
         [Comment("Vehicle Fuel Type")]
@@ -45,39 +46,48 @@ namespace AuctionVehicleProperty.Infrastructure.Data.Models
         [Comment("Vehicle Make")]
         public string Make { get; set; } = string.Empty;
 
+
         [Required]
         [StringLength(50)]
         [Comment("Vehicle Model")]
         public string Model { get; set; } = string.Empty;
 
+
         [Required]
         [Comment("Vehicle year of production")]
         public string Year { get; set; } = string.Empty;
+
 
         [Required]
         [Comment("Vehicle Milage in kilometers")]
         public int Mileage { get; set; }
 
+
         [Required]
         [Comment("Vehicle Power in horse power or in Kw")]
         public int Power { get; set; }
+
 
         [Required]
         [Comment("Vehicle Collor")]
         public string Color { get; set; } = string.Empty;
 
+
         [Required]
         [Comment("Vehicle Price")]
         public decimal Price { get; set; }
+
 
         [Required]
         [Comment("Vehicle Details")]
         [MaxLength(500)]
         public string Details { get; set; } = string.Empty;
 
+
         [Required]
         [Comment("Buisness seller or Individual seller or Dealers")]
         public string SellerId { get; set; } = string.Empty;
+
 
         [ForeignKey(nameof(SellerId))]
         public IdentityUser Seller { get; set; } = null!;
