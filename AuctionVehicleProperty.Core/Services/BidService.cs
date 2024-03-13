f3fb25712b7a87be39c88ddfff6a5762ca9e8bfa
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-03-13 23:29:29 +0200

    Bid Controller and service

---
+++ AuctionVehicleProperty.Core/Services/BidService.cs
@@ -0,0 +1,27 @@
+﻿using AuctionVehicleProperty.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionVehicleProperty.Core.Services
+{
+    public class BidService : IBidService
+    {
+        public Task<bool> AuctionExistsAsync(string auctionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UserExistsAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> VehicleExistsAsync(string vehicleId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AuctionVehicleProperty/Controllers/BidController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionVehicleProperty.Controllers
+{
+    public class BidController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
