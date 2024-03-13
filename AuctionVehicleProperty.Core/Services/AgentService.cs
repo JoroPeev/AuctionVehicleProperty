7061f74524aa803ee7425b95b75cea9aaa4049fc
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-03-13 23:29:09 +0200

    Agent conntroller and service

---
+++ AuctionVehicleProperty.Core/Services/AgentService.cs
@@ -0,0 +1,37 @@
+﻿using AuctionVehicleProperty.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionVehicleProperty.Core.Services
+{
+    public class AgentService : IAgentService
+    {
+        public Task CreateAsync(string userId, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ExistsByIdAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int?> GetAgentIdAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UserHasVehiclesAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UserWithEmailExistsAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
