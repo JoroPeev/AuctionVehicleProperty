b3672724878b94d58a421ee3c33f735ff9d927f7
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-03-13 23:26:06 +0200

    Added Vehicle controller and service

---
+++ AuctionVehicleProperty.Core/Services/VehicleService.cs
@@ -0,0 +1,22 @@
+﻿using AuctionVehicleProperty.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionVehicleProperty.Core.Services
+{
+    public class VehicleService : IVehicleService
+    {
+        public Task<bool> OwnerExistsByIdAsync(string ownerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> VehicleExistsByIdAsync(int vehicleId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
