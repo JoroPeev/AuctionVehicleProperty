bc70399c7475953a982de1ab5343e91b2d702d93
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-03-15 21:23:06 +0200

    Add agent service models

---
+++ AuctionVehicleProperty.Core/Models/Agents/AgentServiceModel.cs
@@ -0,0 +1,22 @@
+﻿using AuctionVehicleProperty.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionVehicleProperty.Core.Models.Agents
+{
+    public class AgentServiceModel
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; } = String.Empty;
+
+        public string Email { get; set; } = String.Empty;
+
+        public string Location { get; set; } = string.Empty;
+
+        public List<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
+    }
+}
---
+++ AuctionVehicleProperty.Core/Models/Agents/AgentUpdateServiceModel.cs
@@ -0,0 +1,17 @@
+﻿using AuctionVehicleProperty.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionVehicleProperty.Core.Models.Agents
+{
+    public class AgentUpdateServiceModel
+    {
+        public string Email { get; set; } = String.Empty;
+        public string Location { get; set; } = string.Empty;
+
+        public List<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
+    }
+}
