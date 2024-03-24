da7658cc17dc0104ae18e06b3b3f507d3f2c9b19
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-03-24 18:54:48 +0200

    Added exeptions

---
+++ AuctionVehicleProperty.Core/Exeptions/ExeptionMessages.cs
@@ -0,0 +1,10 @@
+﻿namespace AuctionVehicleProperty.Core.Exeptions
+{
+    static class ExeptionMessages
+    {
+
+        public const string VehicleNotFound = "Vehicle was not found.";
+
+
+    }
+}
---
+++ AuctionVehicleProperty.Core/Exeptions/VehicleExeption.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionVehicleProperty.Core.Exeptions
+{
+    public class VehicleExeption : Exception
+    {
+
+        public VehicleExeption(){}
+
+        public VehicleExeption(string message)
+           : base(message) { }
+
+    }
+}
