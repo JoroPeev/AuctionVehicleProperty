034dc2cbe53955a10a7deaa0ee6cef120dbc8a06
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-03-25 22:45:13 +0200

    Added agent placin bid model

---
+++ AuctionVehicleProperty.Core/Models/Bids/PlacingBidAgent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionVehicleProperty.Core.Models.Bids
+{
+    public class PlacingBidAgent
+    {
+        public string UserId { get; set; } = string.Empty;
+
+        public decimal Amount { get; set; }
+
+        public DateTime BidTime { get; set; }
+
+
+    }
+}
