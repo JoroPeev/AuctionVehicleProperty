a497745eca7aec2498f63723b2d25a26d6d3409a
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-03-26 23:09:34 +0200

    Added user exeption

---
+++ AuctionVehicleProperty.Core/Exeptions/UserNotFoundExeption.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionVehicleProperty.Core.Exeptions
+{
+    public class UserNotFoundExeption:Exception
+    {
+        public UserNotFoundExeption(){}
+
+        public UserNotFoundExeption(string message) : base(message) { }
+
+
+    }
+}
