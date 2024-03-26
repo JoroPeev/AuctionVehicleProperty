cb20302f5aa65f60e06d8f82a12ebcc5dff3a31b
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-03-26 23:09:12 +0200

    Added more exeption messages

--- AuctionVehicleProperty.Core/Exeptions/ExeptionMessages.cs
+++ AuctionVehicleProperty.Core/Exeptions/ExeptionMessages.cs
@@ -5,6 +5,10 @@
 
         public const string VehicleNotFound = "Vehicle was not found.";
 
-
+        public const string AuctionBiding = "Bid amount must be greater than current highest bid.";
+        
+        public const string AuctionNotFound = "Auction not found."; 
+        
+        public const string UserNotFound = "User not found.";
     }
 }
