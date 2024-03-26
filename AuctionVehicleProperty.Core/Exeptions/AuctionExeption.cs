780878fe664aead2069f8d1a9fbb62e58b0fd56d
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-03-26 23:08:52 +0200

    Auction Custom Exeption Added

---
+++ AuctionVehicleProperty.Core/Exeptions/AuctionExeption.cs
@@ -0,0 +1,10 @@
+﻿namespace AuctionVehicleProperty.Core.Exeptions
+{
+    public class AuctionExeption : Exception
+    {
+        public AuctionExeption(){}
+
+        public AuctionExeption(string message) : base(message){}
+
+    }
+}
