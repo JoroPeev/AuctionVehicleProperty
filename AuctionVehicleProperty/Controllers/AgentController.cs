44142f71900d2c78419a48a9f99d1b837bcc458b
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-03-28 20:30:49 +0200

    Adding auctions in agent

--- AuctionVehicleProperty/Controllers/AgentController.cs
+++ AuctionVehicleProperty/Controllers/AgentController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AuctionVehicleProperty.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AuctionVehicleProperty.Controllers
 {
     public class AgentController : Controller
     {
-        public IActionResult Index()
+        private readonly IAgentService agentService;
+
+        public AgentController(IAgentService _agentService)
+        {
+            agentService = _agentService;
+        }
+
+
+        public IActionResult BecomeAgent()
         {
             return View();
         }
+
+       
     }
 }
