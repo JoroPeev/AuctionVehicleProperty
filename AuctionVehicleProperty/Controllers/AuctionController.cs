58ddf1bbc0bf289c63cdd9c74ad36b02a0ea5c8a
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-03-28 20:30:59 +0200

    Adding auctions in auction

--- AuctionVehicleProperty/Controllers/AuctionController.cs
+++ AuctionVehicleProperty/Controllers/AuctionController.cs
@@ -4,9 +4,72 @@ namespace AuctionVehicleProperty.Controllers
 {
     public class AuctionController : Controller
     {
+       
         public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public IActionResult Details(int id)
         {
             return View();
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
     }
 }
