105d442b804369a3a118e958dd82b4d6b7edcf0a
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-04-04 20:33:35 +0300

    Added masage constants

---
+++ AuctionVehicleProperty.Core/Constants/MessageConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionVehicleProperty.Core.Constants
+{
+    public static class MessageConstants
+    {
+
+        public const string EmailExists = "Email number already exists. Enter another one";
+
+    }
+}
