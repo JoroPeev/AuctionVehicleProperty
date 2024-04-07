5079b4ec9576a32d34b3b0b1fd3a6aa3f1a1c3f5
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-04-07 19:30:37 +0300

    Added must be seller attribute

---
+++ AuctionVehicleProperty/Extensions/MustBeSeller.cs
@@ -0,0 +1,28 @@
+﻿using AuctionVehicleProperty.Controllers;
+using AuctionVehicleProperty.Core.Contracts;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionVehicleProperty.Extensions
+{
+    public class MustBeSeller:ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            IAgentService? agentService = context.HttpContext.RequestServices.GetService<IAgentService>();
+
+            if (agentService == null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            if (agentService != null
+                && agentService.ExistsByIdAsync(context.HttpContext.User.Id()).Result == false)
+            {
+                context.Result = new RedirectToActionResult(nameof(AgentController.Become), "Agent", null);
+            }
+        }
+    }
+}
