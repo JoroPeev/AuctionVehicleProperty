fc9255806bb508adb147e3f4561f71ba5de7e845
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-04-10 23:32:11 +0300

    Added Delete Vehicle view and VehicleDetailsModel

---
+++ AuctionVehicleProperty.Core/Models/Vehicles/VehicleDetailsModel.cs
@@ -0,0 +1,20 @@
+﻿using AuctionVehicleProperty.Infrastructure.Data.Models;
+
+namespace AuctionVehicleProperty.Core.Models.Vehicles
+{
+    public class VehicleDetailsModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string ImageUrls { get; set; } = string.Empty;
+        
+        public string Make { get; set; } = null!;
+
+        public string Model { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }
+
+    }
+}
