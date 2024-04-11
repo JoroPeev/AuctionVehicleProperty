5e60e4b003a97af70bb1d0fc3d63075f7407a110
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-04-11 23:34:49 +0300

    Added BaseController

---
+++ AuctionVehicleProperty/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionVehicleProperty.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {
+    }
+}
