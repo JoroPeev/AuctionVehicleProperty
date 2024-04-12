5f72cc08e4d288e2ac8e33afa2c08279fd0f57b9
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-04-12 21:50:18 +0300

    Added IAucton to aplication services

--- AuctionVehicleProperty/Extensions/ServiceCollectionExtension.cs
+++ AuctionVehicleProperty/Extensions/ServiceCollectionExtension.cs
@@ -15,6 +15,7 @@ namespace Microsoft.Extensions.DependencyInjection
             services.AddScoped<IRepository, Repository>();
             services.AddScoped<IVehicleService, VehicleService>();
             services.AddScoped<IAgentService, AgentService>();
+            services.AddScoped<IAuctionService, AuctionService>();
 
             return services;
         }
