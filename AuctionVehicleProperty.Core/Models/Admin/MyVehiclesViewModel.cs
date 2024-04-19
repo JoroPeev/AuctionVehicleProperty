3c13efe6e7b4ab73b19a7803481710eded695e2e
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-04-19 20:06:14 +0300

    Added MyVehicleViewModel

---
+++ AuctionVehicleProperty.Core/Models/Admin/MyVehiclesViewModel.cs
@@ -0,0 +1,9 @@
+﻿using AuctionVehicleProperty.Core.Models.Vehicles;
+
+namespace AuctionVehicleProperty.Areas.Admin
+{
+    public class MyVehiclesViewModel
+    {
+        public IEnumerable<VehicleServiceModel> AddedVehicles { get; set; } = new List<VehicleServiceModel>();
+    }
+}
