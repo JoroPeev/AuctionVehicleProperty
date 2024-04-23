cad702a2072c73e8271ed8a0b7b144fbf8972118
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-04-23 21:20:24 +0300

    Added more tests

--- AuctionVehicleProperty.Test/AgentServiceTest.cs
+++ AuctionVehicleProperty.Test/AgentServiceTest.cs
@@ -1,14 +1,10 @@
 ﻿using AuctionVehicleProperty.Core.Contracts;
 using AuctionVehicleProperty.Core.Services;
-using AuctionVehicleProperty.Infrastructure.Data.Common;
 using AuctionVehicleProperty.Infrastructure.Data;
+using AuctionVehicleProperty.Infrastructure.Data.Common;
+using AuctionVehicleProperty.Infrastructure.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AuctionVehicleProperty.Test
 {
@@ -41,8 +37,121 @@ namespace AuctionVehicleProperty.Test
             dbContext.Database.EnsureDeleted();
             dbContext.Database.EnsureCreated();
         }
+        [Test]
+        public async Task CreateAsync_Adds_New_Agent_To_Repository()
+        {
+            var user1 = new AppUser
+            { FirstName = "Test", LastName = "Testov" };
+            await repository.AddAsync(user1);
+            await repository.SaveChangesAsync();
 
+            var agent1 = new Agent
+            { Email = "email1@example.com", Location = "Location 1", UserId = user1.Id };
+            
 
+            await agentService.CreateAsync(user1.Id, agent1.Email, agent1.Location);
+
+            var createdAgent = await repository.AllReadOnly<Agent>()
+                .FirstOrDefaultAsync(a => a.UserId == user1.Id && a.Email == agent1.Email && a.Location == agent1.Location);
+
+            Assert.That(createdAgent, Is.Not.Null);
+            Assert.Multiple(() =>
+            {
+                Assert.That(createdAgent.UserId, Is.EqualTo(user1.Id));
+                Assert.That(createdAgent.Email, Is.EqualTo(agent1.Email));
+                Assert.That(createdAgent.Location, Is.EqualTo(agent1.Location));
+            });
+        }
+
+        [Test]
+        public async Task ExistsByIdAsync_Returns_True_If_Agent_Exists()
+        {
+            var user1 = new AppUser
+            { FirstName = "Test", LastName = "Testov" };
+            await repository.AddAsync(user1);
+            await repository.SaveChangesAsync();
+
+            var agent1 = new Agent
+            { Email = "email1@example.com", Location = "Location 1", UserId = user1.Id };
+            await repository.AddAsync(agent1);
+            await repository.SaveChangesAsync();
+
+            var result = await agentService.ExistsByIdAsync(user1.Id);
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public async Task ExistsByIdAsync_Returns_False_If_Agent_Does_Not_Exist()
+        {
+            var userId = "testUserId";
+
+            var result = await agentService.ExistsByIdAsync(userId);
+
+            Assert.IsFalse(result);
+        }
+        [Test]
+        public async Task GetAgentIdAsync_Returns_Agent_Id_If_Exists()
+        {
+            var user1 = new AppUser 
+            { FirstName = "Test", LastName = "Testov" };
+            await repository.AddAsync(user1);
+            await repository.SaveChangesAsync();
+           
+            var agent1 = new Agent 
+            { Email = "email1@example.com", Location = "Location 1", UserId = user1.Id };
+            await repository.AddAsync(agent1);
+            await repository.SaveChangesAsync();
+
+            var result = await agentService.GetAgentIdAsync(user1.Id);
+
+            Assert.That(result, Is.EqualTo(agent1.Id));
+        }
+
+        [Test]
+        public async Task GetAgentIdAsync_Returns_Minus_One_If_Agent_Does_Not_Exist()
+        {
+            var userId = "testUserId"; 
+
+            var result = await agentService.GetAgentIdAsync(userId);
+
+            Assert.That(result, Is.EqualTo(-1));
+        }
+        [Test]
+        public async Task GetAllAgentsAsync_Returns_All_Agents()
+        {
+            var user1 = new AppUser { FirstName = "Test", LastName = "Testov" };
+            var user2 = new AppUser { FirstName = "test2", LastName = "Testoer4" };
+
+            await repository.AddAsync(user1);
+            await repository.AddAsync(user2);
+            await repository.SaveChangesAsync();
+
+            var agent1 = new Agent { Email = "email1@example.com", Location = "Location 1", UserId=user1.Id};
+            var agent2 = new Agent { Email = "email2@example.com", Location = "Location 2", UserId = user2.Id };
+    
+            await repository.AddAsync(agent1);
+            await repository.AddAsync(agent2);
+            await repository.SaveChangesAsync();
+
+            List<Agent> agents = new() { agent1, agent2 };
+
+            var result = await agentService.GetAllAgentsAsync();
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Count(), Is.EqualTo(4));
+            foreach (var agent in agents)
+            {
+                var mappedAgent = result.FirstOrDefault(a => a.Id == agent.Id);
+                Assert.That(mappedAgent, Is.Not.Null);
+                Assert.Multiple(() =>
+                {
+                    Assert.That(mappedAgent.Email, Is.EqualTo(agent.Email));
+                    Assert.That(mappedAgent.Location, Is.EqualTo(agent.Location));
+                });
+            }
+        }
 
 
 
---
+++ AuctionVehicleProperty.Test/BidServceTest.cs
@@ -0,0 +1,121 @@
+﻿using AuctionVehicleProperty.Core.Contracts;
+using AuctionVehicleProperty.Core.Services;
+using AuctionVehicleProperty.Infrastructure.Data.Common;
+using AuctionVehicleProperty.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AuctionVehicleProperty.Infrastructure.Data.Models;
+
+namespace AuctionVehicleProperty.Test
+{
+    [TestFixture]
+    public class BidServceTest
+    {
+        private IRepository repository;
+        private IBidService bidService;
+        private ApplicationDbContext dbContext;
+
+        [SetUp]
+        public void Setup()
+        {
+
+            var configuration = new ConfigurationBuilder()
+                .AddUserSecrets<AuctionServiceTest>()
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("TestConnection");
+
+            var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlServer(connectionString)
+                .Options;
+
+            dbContext = new ApplicationDbContext(contextOptions);
+            repository = new Repository(dbContext);
+            bidService = new BidService(repository);
+
+            dbContext.Database.EnsureDeleted();
+            dbContext.Database.EnsureCreated();
+        }
+        [Test]
+        public async Task AuctionExistsAsync_Returns_True_If_Auction_Exists()
+        {
+            var user = new AppUser { UserName = "TESTYTEST" };
+            await repository.AddAsync(user);
+            await repository.SaveChangesAsync();
+            // Arrange
+            var agent = new Agent
+            {
+                User = user,
+                Email = "test@tes.te",
+                Location = "testTesttest"
+            };
+            await repository.AddAsync(agent);
+            await repository.SaveChangesAsync();
+            var vehicle = new Vehicle
+            {
+                Title = "Test Vehicle",
+                Details = "Test details",
+                Location = "Test location",
+                Mileage = 20000,
+                Power = 200,
+                Make = "Test Make",
+                Model = "Test Model",
+                AverageDivingRange = 600,
+                Price = 60000,
+                Owner = agent,
+                VehicleTypeId = 1,
+                Year = DateTime.MinValue
+            };
+            await repository.AddAsync(vehicle);
+            await repository.SaveChangesAsync();
+            var auctionData = new Auction
+            {
+                StartingPrice = 12344,
+                EndTime = DateTime.MaxValue,
+                StartingTime = DateTime.MinValue,
+                MinimumBidIncrement = 12345,
+                VehicleId = vehicle.Id,
+                CreatorId = agent.Id
+            };
+            await repository.AddAsync(auctionData);
+            await repository.SaveChangesAsync();
+            // Act
+            var result = await bidService.AuctionExistsAsync(auctionData.Id);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public async Task AuctionExistsAsync_Returns_False_If_Auction_Does_Not_Exist()
+        {
+            var nonExistingAuctionId = 13452;
+
+            var result = await bidService.AuctionExistsAsync(nonExistingAuctionId);
+
+            Assert.IsFalse(result);
+        }
+
+
+
+
+
+
+
+
+
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            dbContext.Dispose();
+        }
+    }
+
+}
