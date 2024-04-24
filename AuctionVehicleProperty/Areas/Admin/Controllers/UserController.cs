4fab46a458e61033917d12d3f56b80f52d3188b2
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-04-24 20:53:11 +0300

    Admin userConroller Added and all view

---
+++ AuctionVehicleProperty/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using AuctionVehicleProperty.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionVehicleProperty.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserService userService;
+
+        public UserController(IUserService _userService)
+        {
+            userService = _userService;   
+        }
+
+
+        public async Task<IActionResult> All()
+        {
+            var model = await userService.AllAsync();
+
+            return View(model);
+        }
+    }
+}
