743ea996fb9f4f79c8cd49c88af212314ae9bd18
Joro Peev <107910666+JoroPeev@users.noreply.github.com>
2024-04-29 19:34:08 +0300

    Added NonNegative Attribute

---
+++ AuctionVehicleProperty.Core/Extensions/NonNegativeRequiredAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionVehicleProperty.Core.Extensions
+{
+    public class NonNegativeRequiredAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return new ValidationResult(ErrorMessage ?? "The field is required.");
+            }
+
+            // Check if the value is numeric and less than 0
+            if (IsNumeric(value) && (int)value < 0)
+            {
+                return new ValidationResult(ErrorMessage ?? "Please enter a non-negative value.");
+            }
+
+            return ValidationResult.Success;
+        }
+
+        // Method to check if a value is numeric
+        private bool IsNumeric(object value)
+        {
+            return value is int || value is long || value is decimal || value is float || value is double;
+        }
+    }
+}
